2ad09e43e3c89b3819ef26a1f2a85bebdf9f4d45
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-04-07 21:17:27 +0100

    added profiles before testing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,31 @@ class ProfilesController < ApplicationController
   def index
     @profiles = Profile.all
   end
+
+  def new
+    @profile = Profile.new
+  end
+  
+  def create
+    @profile = Profile.create(options = {:name => profile_params[:name], :user => current_user} )
+    if @restaurant.save  
+      flash[:notice] = 'Profile added successfully'
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end    
+  end
+
+  def restaurant_params
+    params.require(:profile).permit(:name, :image)
+  end  
+
+  def show
+    @profile = Profile.find(params[:id])
+  end  
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
 end
