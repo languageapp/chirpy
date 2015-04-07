fb08dae13ee56320dc0e6a7923e3cb2763e2f8ae
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-8.local>
2015-04-07 19:25:27 +0100

    merge

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ gem 'omniauth-facebook'
 
 gem 'paperclip'
 
+gem 'shoulda'
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ db/migrate/20150407161837_add_profile_id_to_users.rb
@@ -0,0 +1,5 @@
+class AddProfileIdToUsers < ActiveRecord::Migration
+  def change
+    add_reference :users, :profile, index: true, foreign_key: true
+  end
+end
