4cd091d63b5f47b366b07946a9912b750e0c18f4
Kevin Lanzon <kevinlanzon@gmail.com>
2015-04-07 17:35:23 +0100

    three tests passing

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'omniauth-facebook'
 
 gem 'paperclip'
 
+gem 'rails_12factor', group: :production
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,10 +1,47 @@
 require 'rails_helper'
 
-context 'user not signed in and lands on home page' do 
+context 'user not signed in and lands on home page' do
 
-  it 'should display flags' do 
+  it 'should display flags' do
     visit '/'
-    expect(page).to have_css('intro_flags')
-  end  
+    expect(page).to have_css('.english_flag')
+    expect(page).to have_css('.spanish_flag')
+  end
 
-end  
+end
+
+context 'user not signed in and on the log in page' do
+  it 'should see a "login" form and a "sign up" link' do
+    visit('/users/sign_in')
+    expect(page).to have_css('input[type="email"]')
+    expect(page).to have_link('Sign up')
+  end
+
+  it 'should not see "sign out" link' do
+    visit('/users/sign_in')
+    expect(page).not_to have_link('Sign out')
+  end
+
+# context 'user signed in on the profile page' do
+
+#   before do
+#     visit('/users/sign_in')
+#     click_link('Sign up')
+#     fill_in('Email', with: 'test@example.com')
+#     fill_in('Password', with: 'testtest')
+#     fill_in('Password confirmation', with: 'testtest')
+#     click_button('Sign up')
+#   end
+# end
+
+#   it 'should see "sign out" link' do
+#     visit('/users/sign_in')
+#     expect(page).to have_link('Sign out')
+#   end
+
+#   it 'should not see a "sign in" link and a "sign up" link' do
+#     visit('/users/sign_in')
+#     expect(page).not_to have_link('Sign in')
+#     expect(page).not_to have_link('Sign up')
+#   end
+end
