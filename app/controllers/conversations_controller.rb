3f1319851c1a1e8d9df8d0fa0cc18ea8fdc56ac5
Paul Fitzgerald <paulfitz99@gmail.com>
2015-04-09 18:51:17 +0100

    removed header from chat box

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,13 +1,13 @@
 class ConversationsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   # before_filter :authenticate_user!
- 
-  # layout false
-  
+
+  layout false
+
 
   def new
     @conversation = Conversation.new
-  end  
+  end
 
   def create
     if Conversation.between(params[:sender_id],params[:recipient_id]).present?
@@ -17,19 +17,19 @@ class ConversationsController < ApplicationController
     end
     render json: { conversation_id: @conversation.id }
   end
- 
+
   def show
     @conversation = Conversation.find(params[:id])
     @receiver = interlocutor(@conversation)
     @messages = @conversation.messages
     @message = Message.new
   end
- 
+
   private
   def conversation_params
     params.permit(:sender_id, :recipient_id)
   end
- 
+
   def interlocutor(conversation)
     current_user == conversation.recipient ? conversation.sender : conversation.recipient
   end
