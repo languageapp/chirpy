93597cf77379b5cfe6aed15c1583a1e064caad85
Erik Griffin <griffin.erik@gmail.com>
2015-04-13 16:43:56 +0100

    Factored out code

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,11 @@
+(function() {
+  var app = angular.module('chirpy', []);
+
+  app.controller('LanguageFilterController', function() {
+
+    this.test = "Hello, world.";
+
+    this.selectedLanguage = "";
+  });
+
+}());
