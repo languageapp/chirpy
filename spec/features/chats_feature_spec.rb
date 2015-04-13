f98d3249e02f7cbf2c09f0c0456c31039b86bafc
Kevin Lanzon <kevinlanzon@gmail.com>
2015-04-13 15:29:51 +0100

    fixed broken profile tests

--- spec/features/chats_feature_spec.rb
+++ spec/features/chats_feature_spec.rb
@@ -5,7 +5,7 @@ context 'starting a chat' do
   it 'should open a chatbox' do
     sign_up
     create_profile
-    click_button('Message')
+    click_link('Message')
     # expect(page).to have_content 'Joe'
     # expect(current_path).to eq '/profiles/2'
   end
