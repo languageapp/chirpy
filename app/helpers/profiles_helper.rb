7ac5f59a96fc0f48041a14f90a9b32ad9a19d5ab
Erik Griffin <griffin.erik@gmail.com>
2015-04-14 17:07:02 +0100

    First test merge

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,24 @@
 module ProfilesHelper
+
+  def format_language(lang)
+    case lang
+      when 'de', 'German'
+        'Germany'
+      when 'en', 'English'
+        'United-Kingdom'
+      when 'es', 'Spanish'
+        'Spain'
+      when 'sv', 'Swedish'
+        "Sweden"
+      when 'db', 'Norwegian'
+        "Norway"
+      when 'fr', 'French'
+        "France"
+      when 'it', 'Italian'
+        "Italy"
+    else
+      "missing"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  root to: 'profiles#index'
+  scope "(:locale)", :locale => /en|es|de|sv|nb|fr|it/ do
+    devise_for :users
+    root :to => "profiles#index"
+  end
+
   resources :profiles
   resources :languages
 
