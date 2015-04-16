818ee9fa65b0c9a7b2e9706157d65adba36b66a9
Erik Griffin <griffin.erik@gmail.com>
2015-04-16 12:20:58 +0100

    Doubled timeout offline time

--- app/controllers/online_controller.rb
+++ app/controllers/online_controller.rb
@@ -16,7 +16,7 @@ class OnlineController < ApplicationController
   def checkLogout
     timed_out = [];
     @@timeout_hash.each do |key, value|
-      if (Time.now - value) > 3
+      if (Time.now - value) > 6
         timed_out << key
       end
     end
@@ -25,7 +25,7 @@ class OnlineController < ApplicationController
       @@users_online.delete(key.to_s.to_i)
     end
   end
-  
+
   def addOnlineUser(user_id)
     @@users_online << user_id.to_i if !@@users_online.include? user_id.to_i
     @@timeout_hash[user_id.to_sym] = Time.now
