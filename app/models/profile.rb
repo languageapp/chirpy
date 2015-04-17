09475dd16b8e416471e94d88ae8788eba646b7ac
veliancreate <julian_veling@hotmail.com>
2015-04-17 16:54:33 +0100

    prior

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,10 @@
 class Profile < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "logo.png"
+  
+  :s3_host_name => 'chirpy.s3.amazonaws.com'
+  :path => ':filename'
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
